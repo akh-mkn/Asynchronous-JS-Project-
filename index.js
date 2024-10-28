ccefb7b07070657bb6f2a1dce41eeb8f1519a792
Matti Khan <mattikn01@gmail.com>
2024-10-28 17:14:36 +0000

    Update index.js with endpoint variable

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
 
+let endpoint = "https://api.giphy.com/v1/gifs/search?api_key=NdXrQtkBulFxRff7CI9X8saQs68N97Te&q=moon&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
+
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
